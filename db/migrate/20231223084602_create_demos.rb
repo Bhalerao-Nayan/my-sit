af9444dd9e17316907eba3f125a8b8c6c99c6962
Bhalerao.Nayan <bhalerao.nayan899@gmail.com>
2023-12-23 14:27:20 +0530

    adding the initial commit of the project

---
+++ db/migrate/20231223084602_create_demos.rb
@@ -0,0 +1,11 @@
+class CreateDemos < ActiveRecord::Migration[7.1]
+  def change
+    create_table :demos do |t|
+      t.string :name
+      t.string :text
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
